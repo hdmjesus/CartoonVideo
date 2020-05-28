fe5ce68c09df689c0cb92533042a24735238b961
Jesus Hernandez <hdmjesus@gmail.com>
2020-05-28 19:27:12 -0400

    Configuracion del entorno de trabajo

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import HelloWorld from './componets/helloWorld';
+
+ReactDOM.render(<HelloWorld />, document.getElementById('root'));
