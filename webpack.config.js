bf5c4d881a02cc0e8c2ff3ee8577d519836dcb42
Jesus Hernandez <hdmjesus@gmail.com>
2020-10-03 15:28:22 -0400

    Configuraciones finales webpack

--- webpack.config.js
+++ webpack.config.js
@@ -36,7 +36,7 @@ module.exports = {
         ],
       },
       {
-        test: /\.(png|gif|jpg|)$/,
+        test: /\.(png|gif|jpg|svg)$/,
         use: [
           {
             loader: 'file-loader',
